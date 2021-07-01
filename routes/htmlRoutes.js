a0588af7e6a26b82574322287e7d3e886a89a466
Michael Scharf <mwscharf@ucdavis.edu>
2021-07-01 00:36:52 -0700

    changed folder

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router()
+const passport = require('passport')
+const { join } = require('path')
+
+//login is the main index page (index)
+router.get('/login', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'login.html'))
+})
+
+//sends to the register page
+router.get('/register', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'register.html'))
+})
+
+//sends user to their profile page
+router.get('/profile', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'profile.html'))
+})
+
+//sends user to the post page they clicked on
+router.get('/post', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'post.html'))
+})
+
+//sends user to create post page
+router.get('/create', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'create.html'))
+})
+
+//
+router.get('/home', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'home.html'))
+})
+
+router.get('/aboutus', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'aboutus.html'))
+})
+
+
+//export routes
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,6 @@ const router = require('express').Router()
 router.use('/api', require('./userRoutes.js'))
 // other routers go here...
 
+router.use('/', require('./htmlRoutes'))
+
 module.exports = router
