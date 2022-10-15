d56a0abe995fbbc596e3a5c6ab9cf0a3a85ad09c
Michael Scharf <mwscharf@ucdavis.edu>
2022-10-14 23:50:25 -0700

    added api to search

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -20,23 +20,20 @@ const renderCards = () => {
         return;
       }
       data.forEach((post) => {
-        if (post.isDraft === true) {
-          ("");
-        } else if (post.song !== undefined) {
-          // console.log(post, 'this is post')
+        axios
+          .get(
+            `https://theaudiodb.com/api/v1/json/523532/search.php?s=${post.artist}`
+          )
+          .then((res) => {
+            let artist = res.data;
 
-          // axios.get(`https://theaudiodb.com/api/v1/json/1/search.php?s=${post.artist}`)
-          //   .then(res => {
-          // let artist = res.data
-          // console.log(artist,  'this is artist')
-
-          // if (artist.artists === null) {
-          document.getElementById("renderCards").innerHTML += `
+            if (artist.artists == null) {
+              document.getElementById("renderCards").innerHTML += `
 
                 <div class="col s12 m6 l4">
                 <div class="card #424242 grey darken-3 transparent hoverable">
                 <div class="card-image waves-effect">
-                <img class="viewPostImg" src='/assets/images/angel.webp' data-id=${post.id} alt="image not found">
+                <img class="viewPostImg" src='/assets/images/angel.webp' data-id=${post.id} alt="image of an angel">
                 </div>
                 <div class="card-content center-align">
 
@@ -45,39 +42,23 @@ const renderCards = () => {
                 </div>
                 </div>
                 `;
-          // } else {
-          //   document.getElementById('renderCards').innerHTML += `
+            } else {
+              document.getElementById("renderCards").innerHTML += `
 
-          // <div class="col s12 m6 l4">
-          // <div class="card #424242 grey darken-3 transparent hoverable">
-          // <div class="card-image waves-effect">
-          // <img  class="viewPostImg" src=${artist.artists[0].strArtistThumb} data-id=${post.id} alt="image not found">
-          // </div>
-          // <div class="card-content center-align">
-          // <span class="card-title  white-text text-darken-4 myTitle truncate">${post.song} <br> ${post.artist}</span>
-          // <a class="waves-effect waves-light btn viewPost black myBtn" data-id=${post.id}>Tab</a>
-          // </div>
-          // </div>
-          // `
-          // }
-          //end forEach
-          //     })
-        } else {
-          document.getElementById("renderCards").innerHTML += `
-
-                <div class="col s12 m6 l4">
-                <div class="card #424242 grey darken-3 transparent hoverable">
-                <div class="card-image waves-effect">
-                <img class="viewUserImg" src='/assets/images/angel-913630_1280.webp' data-username=${post.username} alt="image not found">
-                </div>
-                <div class="card-content center-align">
-
-                <span class="card-title activator white-text text-darken-4 myTitle truncate">User <br> ${post.username}</span>
-                <a class="waves-effect waves-light btn viewUser black myBtn"  data-username=${post.username}>Profile</a>
-                </div>
-                </div>
-                `;
-        }
+        <div class="col s12 m6 l4">
+        <div class="card #424242 grey darken-3 transparent hoverable">
+        <div class="card-image waves-effect">
+        <img  class="viewPostImg" src=${artist.artists[0].strArtistThumb} data-id=${post.id} alt="image not found">
+        </div>
+        <div class="card-content center-align">
+        <span class="card-title  white-text text-darken-4 myTitle truncate">${post.song} <br> ${post.artist}</span>
+        <a class="waves-effect waves-light btn viewPost black myBtn" data-id=${post.id}>Tab</a>
+        </div>
+        </div>
+        `;
+            }
+            // end forEach
+          });
       });
     })
     .catch((err) => console.log(err));
